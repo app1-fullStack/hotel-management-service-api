f77502c25deb5530218cd2660b2135d08c9cacfb
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-08-13 10:09:50 +0530

    first commit

---
+++ hotel-management-service-api/hotel-management-service-api/src/test/java/com/app/hotel_system/hotel_management_service_api/HotelManagementServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hotel_system.hotel_management_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
