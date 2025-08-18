2f144c745f957e7f7d5197d0c9da3c1ab24f5db1
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-08-18 09:38:56 +0530

    database initiated

---
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/entity/Hotel.java
@@ -0,0 +1,40 @@
+package com.hotel_management_system_api.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="hotel")
+public class Hotel {
+    @Id
+    @Column(name="hotel_id")
+    private String hotelId;
+
+    @Column(name="hotel_name", nullable=false, length=100)
+    private String hotelName;
+
+    @Column(name="star_rating", nullable=false)
+    private int star_rating;
+
+    @Column( nullable=false)
+    @Lob
+    private Blob description;
+
+    @Column(name="created_at", nullable=false)
+    private LocalDateTime created_at;
+
+    @Column(name="updated_at", nullable=false)
+    private LocalDateTime updated_at;
+
+    @Column(name="activeStatus")
+    private boolean activeStatus;
+
+    @Column(name="startingFrom")
+    private BigDecimal startingFrom;
+
+
+
+}
