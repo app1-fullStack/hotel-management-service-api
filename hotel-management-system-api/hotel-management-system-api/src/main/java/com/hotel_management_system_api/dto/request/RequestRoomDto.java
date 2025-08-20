e4aa134bc56918d77eedd056fee986254c525eb1
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-08-20 09:59:20 +0530

    request dto done

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/request/RequestRoomDto.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/request/RequestRoomDto.java
@@ -1,4 +1,12 @@
 package com.hotel_management_system_api.dto.request;
 
+import java.math.BigDecimal;
+
 public class RequestRoomDto {
+    private int bedCount;
+    private boolean isAvailable;
+    private BigDecimal price;
+    private String roomNumber;
+    private String roomType;
+    private String branchId;
 }
--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/request/RequestRoomImageDto.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/request/RequestRoomImageDto.java
@@ -1,4 +1,14 @@
 package com.hotel_management_system_api.dto.request;
 
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RequestRoomImageDto {
+    private MultipartFile file;
+    private String roomId;
 }
