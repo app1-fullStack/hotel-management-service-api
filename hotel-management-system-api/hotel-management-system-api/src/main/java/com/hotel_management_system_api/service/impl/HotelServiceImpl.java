25643e947d8feb3341a667065eb2aff83bb0bb30
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-08-27 16:52:30 +0530

    modified some api files

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/impl/HotelServiceImpl.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/impl/HotelServiceImpl.java
@@ -3,14 +3,24 @@ package com.hotel_management_system_api.service.impl;
 import com.hotel_management_system_api.dto.request.RequestHotelDto;
 import com.hotel_management_system_api.dto.response.ResponseHotelDto;
 import com.hotel_management_system_api.dto.response.paginate.HotelPaginateResponseDto;
+import com.hotel_management_system_api.entity.Hotel;
+import com.hotel_management_system_api.repo.HotelRepo;
 import com.hotel_management_system_api.service.HotelService;
+import lombok.*;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class HotelServiceImpl implements HotelService {
     @Override
     public void create(RequestHotelDto dto) {
 
+        private final HotelRepo hotelRepo;
     }
 
     @Override
@@ -31,5 +41,10 @@ public class HotelServiceImpl implements HotelService {
     @Override
     public HotelPaginateResponseDto findAll(int page, int size, String searchText) {
         return null;
+
+        private Hotel toHotel(RequestHotelDto dto) {
+            return dto==null?null:
+                    Hotel
+        }
     }
 }
