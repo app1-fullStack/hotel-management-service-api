636a5067c790b3a3e2c381eca075f2aa73616dcc
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-10-23 11:55:35 +0530

    added util folder and with some implementation

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/impl/HotelServiceImpl.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/impl/HotelServiceImpl.java
@@ -3,11 +3,16 @@ package com.hotel_management_system_api.service.impl;
 import com.hotel_management_system_api.dto.request.RequestHotelDto;
 import com.hotel_management_system_api.dto.response.ResponseHotelDto;
 import com.hotel_management_system_api.dto.response.paginate.HotelPaginateResponseDto;
+import com.hotel_management_system_api.entity.Hotel;
 import com.hotel_management_system_api.service.HotelService;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+
 @Service
 public class HotelServiceImpl implements HotelService {
+
     @Override
     public void create(RequestHotelDto dto) {
 
@@ -31,5 +36,17 @@ public class HotelServiceImpl implements HotelService {
     @Override
     public HotelPaginateResponseDto findAll(int page, int size, String searchText) {
         return null;
+        }
+
+
+    //map structs, model mappers
+    private Hotel toHotel(RequestHotelDto dto){
+        return dto == null?null:
+                Hotel.builder()
+                        .hotelName(dto.getHotelName())
+                        .hotelId(UUID.randomUUID().toString())
+                        .starRating(dto.getStarRating())
+                        .description(dto.getDescription())
+                        .build();
     }
 }
