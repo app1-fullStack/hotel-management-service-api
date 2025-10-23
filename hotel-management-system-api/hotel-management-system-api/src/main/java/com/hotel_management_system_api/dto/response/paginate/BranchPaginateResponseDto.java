d5ff6e154b653ec86d66d3b6d182823fc5a1dd27
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-10-23 11:17:58 +0530

    files checked and fix some errors in the codes

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/response/paginate/BranchPaginateResponseDto.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/response/paginate/BranchPaginateResponseDto.java
@@ -1,9 +1,15 @@
 package com.hotel_management_system_api.dto.response.paginate;
 
 import com.hotel_management_system_api.dto.response.ResponseBranchDto;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BranchPaginateResponseDto {
     private List<ResponseBranchDto> dataList;
     private long dataCount;
--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/response/paginate/HotelPaginateResponseDto.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/dto/response/paginate/HotelPaginateResponseDto.java
@@ -1,9 +1,15 @@
 package com.hotel_management_system_api.dto.response.paginate;
 
 import com.hotel_management_system_api.dto.response.ResponseHotelDto;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class HotelPaginateResponseDto {
     private List<ResponseHotelDto> dataList;
     private long dataCount;
