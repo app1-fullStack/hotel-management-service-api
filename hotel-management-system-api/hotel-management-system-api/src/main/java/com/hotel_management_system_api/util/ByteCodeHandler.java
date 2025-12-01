832b2b2b1db4955862985162c8c945fbb1c6bf5b
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-12-01 10:14:26 +0530

    add: implement ByteCodeHandlerService for handling byte code operations

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/util/ByteCodeHandler.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/util/ByteCodeHandler.java
@@ -1,10 +1,18 @@
 package com.hotel_management_system_api.util;
 
+import org.springframework.stereotype.Service;
+
 import javax.sql.rowset.serial.SerialBlob;
 import java.nio.charset.StandardCharsets;
 import java.sql.Blob;
 import java.sql.SQLException;
 
+
+
+@Service
+public static class ByteCodeHandlerService {
+
+}
 public class ByteCodeHandler {
     public Blob stringToBlob(String s) throws SQLException {
         if (s == null) {
