72d05cef7a109ea052974de51a34a92f7960ef7a
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-12-19 12:17:17 +0530

    fix: update HotelService interface to declare SQLException for create and findById methods

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/HotelService.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/service/HotelService.java
@@ -5,12 +5,13 @@ import com.hotel_management_system_api.dto.response.ResponseHotelDto;
 import com.hotel_management_system_api.dto.response.paginate.HotelPaginateResponseDto;
 import org.apache.coyote.Request;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface HotelService {
-    public void create(RequestHotelDto dto);
+    public void create(RequestHotelDto dto) throws SQLException;
     public void update(RequestHotelDto dto, String hotelId);
     public void delete(String hotelId);
-    public ResponseHotelDto findById(String hotelId);
+    public ResponseHotelDto findById(String hotelId) throws SQLException;
     public HotelPaginateResponseDto findAll(int page, int size, String searchText);
 }
