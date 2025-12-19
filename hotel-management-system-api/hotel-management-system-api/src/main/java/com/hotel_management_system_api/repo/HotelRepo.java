5468d78b64242cdfe73506b2d52d1a274bee9920
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-12-19 13:39:07 +0530

    add: implement search and count methods in HotelRepo for active hotels

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/repo/HotelRepo.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/repo/HotelRepo.java
@@ -1,7 +1,21 @@
 package com.hotel_management_system_api.repo;
 
 import com.hotel_management_system_api.entity.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HotelRepo extends JpaRepository<Hotel,String> {
+
+    @Query(value = "SELECT * FROM hotel WHERE hotel_name LIKE %?1% AND active_status = true", nativeQuery = true)
+    Page<Hotel> searchHotels(String searchText, Pageable pageable);
+
+    @Query(value = "SELECT COUNT(*) FROM hotel WHERE hotel_name LIKE %?1% AND active_status = true", nativeQuery = true)
+    long countAllHotels(String searchText);
+
+    @Query(value = "SELECT * FROM hotel WHERE hotel_name LIKE %?1% AND active_status = true", nativeQuery = true)
+    Page<Hotel> searchAllHotels(String searchText, Pageable pageable);
 }
+
