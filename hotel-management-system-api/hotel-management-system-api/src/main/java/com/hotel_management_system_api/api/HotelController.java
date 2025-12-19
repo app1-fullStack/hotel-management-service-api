b2af8b823f14f53fbe951ea9caeeb5dc2a45ca2a
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-12-19 14:01:30 +0530

    add: implement search and count methods in HotelRepo for active hotels

--- hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/api/HotelController.java
+++ hotel-management-system-api/hotel-management-system-api/src/main/java/com/hotel_management_system_api/api/HotelController.java
@@ -1,9 +1,32 @@
 package com.hotel_management_system_api.api;
 
+import com.hotel_management_system_api.dto.request.RequestHotelDto;
+import com.hotel_management_system_api.service.HotelService;
+import com.hotel_management_system_api.util.StandardResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/hotel-management/api/v1/hotels")
 public class HotelController {
+    private final HotelService hotelService;
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @PostMapping("/user/create")
+    public ResponseEntity<StandardResponseDto> create(
+            @RequestBody RequestHotelDto dto) throws SQLException {
+        hotelService.create(dto);
+        return new ResponseEntity<> (
+                new StandardResponseDto(201,
+                        "Hotel created successfully",
+                        null),HttpStatus.CREATED);
+    }
 }
